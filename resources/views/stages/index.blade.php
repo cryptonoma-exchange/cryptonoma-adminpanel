77ae56f9af8ca4810263c5e54a76cdd07cf66005
Abdul Qadeer <abdul@cidrusmoney.com>
2024-02-26 16:27:03 +0500

    success and error messages updated
    success and error messages updated.

--- resources/views/stages/index.blade.php
+++ resources/views/stages/index.blade.php
@@ -6,10 +6,36 @@
         <div class="main-panel">
         <div class="content">
             <div class="page-inner">
-                <x-danger-alert/>
-                <x-success-alert/>
-                <!-- Beginning of  Dashboard Stats  -->
+               
                 <div class="row">
+
+                    <div class="col-lg-12">
+        
+                        @if ($errors->any())
+                                <div class="alert alert-danger">
+                                    <ul>
+                                        @foreach ($errors->all() as $error)
+                                            <li>{{ $error }}</li>
+                                        @endforeach
+                                    </ul>
+                                </div>
+                            @endif
+
+
+                            @if(session('success'))
+                                <div class="alert alert-success">
+                                    {{ session('success') }}
+                                </div>
+                            @endif
+
+                            @if(session('error'))
+                                <div class="alert alert-danger">
+                                    {{ session('error') }}
+                                </div>
+                            @endif
+
+                    </div>
+                    
                     <div class="col-md-12">
                         <div class="card">
                             <div class="card-body">
